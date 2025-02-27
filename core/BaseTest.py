c79890e012cf20ef0f9045ddc14e74a9c5643a6d
Vadim Velichko <super.velik.123@mail.ru>
2025-02-28 00:15:47 +0300

    Write first auto tests for Login form with PageObject pattern

--- core/BaseTest.py
+++ core/BaseTest.py
@@ -3,6 +3,6 @@ from selenium import webdriver
 
 @pytest.fixture(scope='session')
 def browser():
-    driver = webdriver.Chrome()
-    yield driver
+    driver = webdriver.Chrome() # запустили браузер и пускай дальше webdriver с ним работает и делает, что хочет
+    yield driver # поставили на паузу и не продолжать код, сессия завершилась - завершился браузер 👇
     driver.quit()
