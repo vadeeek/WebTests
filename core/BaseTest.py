4ad39a4c0f3158b3463393728bb4016fc3206878
Vadim Velichko <super.velik.123@mail.ru>
2025-03-12 01:48:12 +0300

    Подготовка к Selenium Grid

--- core/BaseTest.py
+++ core/BaseTest.py
@@ -4,6 +4,9 @@ from selenium import webdriver
 
 @pytest.fixture(scope='session')
 def browser():
-    driver = webdriver.Chrome() # запустили браузер и пускай дальше webdriver с ним работает и делает, что хочет
+    options = webdriver.ChromeOptions()
+    options.add_argument("--lang=ru")
+    driver = webdriver.Remote(command_executor="http://31.129.111.252:4444", options=options)
     yield driver # поставили на паузу и не продолжать код, сессия завершилась - завершился браузер 👇
-    driver.quit()
+    if driver:
+        driver.quit()
