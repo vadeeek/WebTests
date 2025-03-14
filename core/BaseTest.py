a520a31423d3d05111e7feed934768d983307cce
Vadim Velichko <super.velik.123@mail.ru>
2025-03-15 00:08:01 +0300

    Сменил ip:port на TeamCity

--- core/BaseTest.py
+++ core/BaseTest.py
@@ -6,7 +6,7 @@ from selenium import webdriver
 def browser():
     options = webdriver.ChromeOptions()
     options.add_argument("--lang=ru")
-    driver = webdriver.Remote(command_executor="http://31.129.111.252:4444", options=options)
+    driver = webdriver.Remote(command_executor="http://31.129.111.252:8111", options=options)
     yield driver # поставили на паузу и не продолжать код, сессия завершилась - завершился браузер 👇
     if driver:
         driver.quit()
